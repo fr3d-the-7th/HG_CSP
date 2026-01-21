0bc6f47641997e3d47ad6ecf02a4b0363a2aa6fd
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-01-21 13:43:39 -0700

    varables notes

--- notes/variables.py
+++ notes/variables.py
@@ -1,3 +1,12 @@
 name = "Fred"
 
 print(name)
+
+Money = -7864
+pie = input("What is your favorate kind of pie? ")
+
+Time = "1:25 PM"
+
+print(Time)
+print("programing is the best")
+print(Time, "is", pie, "time")

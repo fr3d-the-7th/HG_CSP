f6d40413e860e4c9247ba6105c2b79417b799056
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-01-22 13:17:36 -0700

    First Program

---
+++ practices/first_program.py
@@ -0,0 +1,5 @@
+# HG First Program
+
+name = input("What is your name? ")
+
+print("Have a fantastic day " + name + "!")
---
+++ practices/variables_practice.py
@@ -0,0 +1,19 @@
+# HG  Variables Practice
+
+name = input
+
+small_number
+
+big_number = 
+
+breakfeast = 
+
+print(name)
+
+print()
+
+print()
+
+print()
+
+print()

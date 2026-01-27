90008ccf0273cfe95f8847b7ba8790b6e5cac987
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-01-27 19:34:20 +0000

    1/28

--- practices/Financial_Calculator.py
+++ practices/Financial_Calculator.py
@@ -13,13 +13,13 @@ vroom = float(input("what is your monthly transportation cost?: $"))
 extra = income - (home + util + food + vroom)
 
 
-print("since you hosing cost is $" + str(home) + " it consumes " + str(home/income*100) + "% of your income.")
+print("Since you housing cost is $" + str(home) + ", it consumes " + str(round(home/income*100)) + "% of your income.")
 
-print("since you utilities cost is $" + str(util) + " it consumes" + str(util/income*100) + "% of your income.")
+print("Since you utilities cost is $" + str(util) + ", it consumes " + str(round(util/income*100)) + "% of your income.")
 
-print("since you groceries buget is $" + str(food) + " it consumes" + str(food/income*100) + "% of your income.")
+print("Since you groceries buget is $" + str(food) + ", it consumes " + str(round(food/income*100)) + "% of your income.")
 
-print("since you transportation cost is $" + str(vroom) + " it consumes" + str(vroom/income*100) + "% of your income.")
+print("Since you transportation cost is $" + str(vroom) + ", it consumes " + str(round(vroom/income*100)) + "% of your income.")
 
-print("you have $" + str(extra) + "left after all your normal expenses. It is highly recomended to put it into savings.")
+print("You have $" + str(extra) + "left after all your normal expenses. It is highly recomended to put it into savings or try to pay off any debts.")
 

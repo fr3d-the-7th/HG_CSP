e77b309f3c865c024cf3274179dce311540e9c7a
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-01-27 13:44:02 -0700

    1/28.7

---
+++ notes/string_notes.py
@@ -0,0 +1,16 @@
+# HG String notes
+
+name = input("what is thy name?: ").strip().upper()
+
+print("Hello, ", name[0])
+
+sentence = "the quick brown fox jumps over the lazy dog."
+print()
+word = input("pick a word from the sentence to change: ").strip().lower()
+
+new_word = input("what is the new word?: ").strip().lower()
+
+start = sentence.find(word)
+sentence = sentence.replace(word, new_word)
+print(name + "! Did you know? " sentence)
+

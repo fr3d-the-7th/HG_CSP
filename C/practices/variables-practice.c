795aeeaeeb4be9b8c8d0c9433a359bcf9ffe760f
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-02-19 13:44:11 -0700

    2/19/2

---
+++ C/practices/variables-practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    char human[] = "fred";
+    int lucky = 6;
+    float gpa = 3.4;
+    char nom[] = "pankakes";
+    char color[] = "barf green";
+    char jail[] = "Frankfort High";
+    char year[] = "2016";
+    char eye_color[] = "brown";
+    int oldyness_percentage = 9;
+    char period[] = "recess";
+);
+  printf("%s, It's %d, highly advise you to quit your behavior; I know who you are.\n Your eyes are %s. You are %d years old. Your favorite color is %s, and your lucky number is %d");
+}

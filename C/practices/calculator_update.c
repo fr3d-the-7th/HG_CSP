9bdabd75594dec26fa430228bd029b459fa448b8
fr3d-the-7th <hope.gower@ucas-edu.net>
2026-03-06 13:38:55 -0700

    2

--- C/practices/calculator_update.c
+++ C/practices/calculator_update.c
@@ -1,21 +1,34 @@
-//HG Financial Calculator - c
+//HG 6 Financial Calculator - c
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-int get(char* subject){
-    int bill;
-    printf("Tell me your monthly %s: \n", subject);
-    scanf("%f", &income);
-
-
-
+float get(char* subject){
+    float bill;
+    printf("Tell me your monthly %s: $\n", subject);
+    scanf("%f", &bill);
+    return bill;
 }
 
+float calcu(float bucks, float income){
+float percent = bucks/income*100;
+
+return percent;
+}
 int main(){
+float income = get("income");
+float util = get("utilitiles bill");
+float food = get("grocery buget");
+float vroom = get("transpotation cost");
+float save = get("investment into saving");
+float extra = income-util-food-vroom-save;
 
-
-
-
+printf("Your monthly income is $%.2f\n", income);
+printf("your monthly utilitiles bill is %.2f percent of your income.\n", calcu(util, income));
+printf("your monthly grocery buget is %.2f percent of your income.\n", calcu(food, income));
+printf("your monthly transpotation cost is %.2f percent of your income.\n", calcu(vroom, income));
+printf("your monthly investment into saving is %.2f percent of your income.\n", calcu(save, income));
+printf("You have $%.2f left thats %.2f percent of your income! Enjoy it or pay off any debts! :)\n", extra, calcu(extra, income));
 return 0;
 }

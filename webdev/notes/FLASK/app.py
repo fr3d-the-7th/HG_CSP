125259bacc498fa0f9c2f4b3da707fa948342919
fred-the-7th <hope.gower@ucas-edu.net>
2026-04-24 13:44:05 -0600

    gjhgnb

---
+++ webdev/notes/FLASK/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, url_for, render_template
+app = Flask(__name__)
+admin = True
+@app.route("/")
+
+def home():
+    return render_template("index.html", heading= "Home Page", content= "")
+
+
+@ app.route("/test")
+def testing():
+    return render_template("index.html", heading= "test Page", content= "this is not a drill!")
+
+@ app.route("/admin")
+def administrator():
+    if admin:
+        return "You are and administrator!"
+
+    else: 
+         return redirect(url_for("home"))
+
+
+
+@ app.route("/<name>")
+
+def hello(name):
+    return f"Hello {name}!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
+
+
+
